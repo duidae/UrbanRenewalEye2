22c0360c9a2f7e5be1c6034b4cc71087b0932653
duidae <duidae@gmail.com>
2021-08-01 21:19:14 +0800

    Init project

---
+++ src/components/Map.tsx
@@ -0,0 +1,50 @@
+import {Component} from "react";
+import GoogleMapReact from "google-map-react";
+import "./Map.scss";
+
+export class Map extends Component {
+    private loadGeojson = (map: any, maps: any) => {
+        map.data.setStyle((feature: any) => {
+            const color = feature.getProperty("status") === "有效" ? "green" : "gray";
+            return {
+                fillColor: color,
+                strokeColor: color,
+                clickable: true
+            };
+        });
+
+        map.data.loadGeoJson("renewalGeojson.json");
+
+        // Click event
+        map.data.addListener("click", (event: any) => {
+            const content = `${event.feature.getProperty("id")}<br>${event.feature.getProperty("type")}<br>${event.feature.getProperty("status")}`;
+            const clickInfoWindow = new google.maps.InfoWindow({position: event.latLng, content: content});
+            clickInfoWindow.open({map, shouldFocus: false});
+        });
+
+        // Hover event
+        let mouseoverInfoWindow: any;
+        map.data.addListener("mouseover", (event: any) => {
+            const content = `${event.feature.getProperty("id")}<br>${event.feature.getProperty("type")}<br>${event.feature.getProperty("status")}`;
+            mouseoverInfoWindow = new google.maps.InfoWindow({position: event.latLng, content: content});
+            mouseoverInfoWindow.open({map, shouldFocus: false});
+        });
+        map.data.addListener("mouseout", (event: any) => {
+            mouseoverInfoWindow?.close();
+        });
+    };
+
+    render() {
+        return (
+            <div className="map">
+                <GoogleMapReact
+                    defaultCenter={{lat: 25.036963137555553, lng: 121.52847908553798}}
+                    defaultZoom={17}
+                    options={{streetViewControl: true, mapTypeControl: true}}
+                    yesIWantToUseGoogleMapApiInternals={true}
+                    onGoogleApiLoaded={({map, maps}) => this.loadGeojson(map, maps)}
+                ></GoogleMapReact>
+            </div>
+        );
+    }
+}
