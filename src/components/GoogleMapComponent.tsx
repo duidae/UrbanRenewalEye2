fc7a9373262ddf60a05011762755ff4eb43ac1b3
duidae <duidae@gmail.com>
2021-08-08 15:53:44 +0800

    Extract control from map

--- src/components/GoogleMapComponent.tsx
+++ src/components/GoogleMapComponent.tsx
@@ -1,37 +1,25 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import GoogleMapReact from "google-map-react";
 import {Fab} from "@material-ui/core";
 import LayersIcon from "@material-ui/icons/Layers";
 import "./GoogleMapComponent.scss";
 
-import {Divider, IconButton, InputBase, Paper} from "@material-ui/core";
-import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
-import StreetviewIcon from "@material-ui/icons/Streetview";
-import CenterFocusStrongIcon from "@material-ui/icons/CenterFocusStrong";
-import DirectionsIcon from '@material-ui/icons/Directions';
-
-import {AppStore} from "stores";
-// import {MapControlComponent} from ".";
+import {MapControlComponent} from ".";
 
 const TAIPEI_CENTER: GoogleMapReact.Coords = {lat: 25.038357847174, lng: 121.54770626982};
 const RENEWAL_GEOJSON = "renewalUnits_sample.json";
 
 export class GoogleMapComponent extends React.Component {
-    private map;
-    private mapControlRef;
-    private adRef;
-    private layerPanelRef;
-
     private loadMap = (map: any, maps: any) => {
         this.initMap(map, maps);
         this.loadData(map, maps);
     };
 
     private initMap = (map: any, maps: any) => {
-        this.map = map;
-        map.controls[google.maps.ControlPosition.TOP_CENTER].push(this.mapControlRef);
-        map.controls[google.maps.ControlPosition.BOTTOM_LEFT].push(this.layerPanelRef);
+        const mapControl = document.createElement("div");
+        ReactDOM.render(<MapControlComponent map={map} />, mapControl);
+        map.controls[google.maps.ControlPosition.TOP_CENTER].push(mapControl);
     };
 
     private loadData = (map: any, maps: any) => {
@@ -65,34 +53,6 @@ export class GoogleMapComponent extends React.Component {
         });
     };
 
-    private handleLocateMeClick = () => {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(
-                (position: GeolocationPosition) => {
-                    const pos = {
-                        lat: position.coords.latitude,
-                        lng: position.coords.longitude
-                    };
-                    if (this.map) {
-                        this.map.setCenter(pos);
-                        const infoWindow = new google.maps.InfoWindow({position: pos, content: "我在這裡！"});
-                        infoWindow?.open({map: this.map, shouldFocus: false});
-                    }
-                },
-                () => {
-                    console.log("Error: The Geolocation service failed.");
-                }
-            );
-        } else {
-            // Browser doesn't support Geolocation
-            console.log("Error: Your browser doesn't support geolocation.");
-        }
-    };
-
-    private handleSideBySideModeClick = () => {
-        AppStore.Instance.setSideBySideMode();
-    };
-
     render() {
         return (
             <div className="map">
@@ -103,29 +63,7 @@ export class GoogleMapComponent extends React.Component {
                     yesIWantToUseGoogleMapApiInternals={true}
                     onGoogleApiLoaded={({map, maps}) => this.loadMap(map, maps)}
                 />
-                <Paper ref={ref => (this.mapControlRef = ref)} component="form">
-                    <IconButton aria-label="menu">
-                        <MenuIcon />
-                    </IconButton>
-                    <InputBase
-                        placeholder="搜尋地址..."
-                        inputProps={{'aria-label': 'search google maps'}}
-                    />
-                    <IconButton aria-label="search">
-                        <SearchIcon />
-                    </IconButton>
-                    <Divider orientation="vertical" />
-                    <IconButton color="primary" aria-label="directions">
-                        <DirectionsIcon />
-                    </IconButton>
-                    <IconButton onClick={this.handleLocateMeClick} color="primary" aria-label="Locate Me">
-                        <CenterFocusStrongIcon />
-                    </IconButton>
-                    <IconButton onClick={this.handleSideBySideModeClick} color="primary" aria-label="Side by side mode">
-                        <StreetviewIcon />
-                    </IconButton>
-                </Paper>
-                <Fab ref={ref => (this.layerPanelRef = ref)} variant="extended">
+                <Fab variant="extended">
                     <LayersIcon />
                     Layer
                 </Fab>
--- src/components/MapControlComponent.tsx
+++ src/components/MapControlComponent.tsx
@@ -2,59 +2,86 @@ import React from "react";
 import {observer} from "mobx-react";
 import {Divider, IconButton, InputBase, Paper} from "@material-ui/core";
 import {withStyles} from "@material-ui/core/styles";
-import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
+import MenuIcon from "@material-ui/icons/Menu";
+import SearchIcon from "@material-ui/icons/Search";
 import StreetviewIcon from "@material-ui/icons/Streetview";
 import CenterFocusStrongIcon from "@material-ui/icons/CenterFocusStrong";
-import DirectionsIcon from '@material-ui/icons/Directions';
+import DirectionsIcon from "@material-ui/icons/Directions";
+
+import {AppStore} from "stores";
 
 const styles = theme => ({
-  root: {
-    padding: '2px 4px',
-    display: 'flex',
-    alignItems: 'center',
-    width: 400,
-  },
-  input: {
-    marginLeft: theme.spacing(1),
-    flex: 1,
-  },
-  iconButton: {
-    padding: 10,
-  },
-  divider: {
-    height: 28,
-    margin: 4,
-  },
+    root: {
+        margin: 10,
+        display: "flex",
+        alignItems: "center",
+        width: 400,
+        height: 40
+    },
+    input: {
+        marginLeft: theme.spacing(1),
+        flex: 1
+    },
+    iconButton: {
+        padding: 10
+    },
+    divider: {
+        height: 28,
+        margin: 4
+    }
 });
 
 @observer
 class MapControl extends React.Component<any, any> {
+    private handleLocateMeClick = () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position: GeolocationPosition) => {
+                    const pos = {
+                        lat: position.coords.latitude,
+                        lng: position.coords.longitude
+                    };
+                    if (this.props.map) {
+                        this.props.map.setCenter(pos);
+                        const infoWindow = new google.maps.InfoWindow({position: pos, content: "我在這裡！"});
+                        infoWindow?.open({map: this.props.map, shouldFocus: false});
+                    }
+                },
+                () => {
+                    console.log("Error: The Geolocation service failed.");
+                }
+            );
+        } else {
+            // Browser doesn't support Geolocation
+            console.log("Error: Your browser doesn't support geolocation.");
+        }
+    };
+
+    private handleSideBySideModeClick = () => {
+        AppStore.Instance.setSideBySideMode();
+    };
+
     public render() {
         const classes = this.props.classes;
         return (
             <Paper component="form" className={classes.root}>
-              <IconButton className={classes.iconButton} aria-label="menu">
-                <MenuIcon />
-              </IconButton>
-              <InputBase
-                className={classes.input}
-                placeholder="Search Google Maps"
-                inputProps={{ 'aria-label': 'search google maps' }}
-              />
-              <IconButton type="submit" className={classes.iconButton} aria-label="search">
-                <SearchIcon />
-              </IconButton>
-              <Divider className={classes.divider} orientation="vertical" />
-              <IconButton color="primary" className={classes.iconButton} aria-label="directions">
-                <DirectionsIcon />
-              </IconButton>
-              <IconButton color="primary" className={classes.iconButton} aria-label="directions">
-                <CenterFocusStrongIcon />
-              </IconButton>
-              <IconButton color="primary" className={classes.iconButton} aria-label="directions">
-                <StreetviewIcon />
-              </IconButton>
+                <IconButton className={classes.iconButton} aria-label="menu">
+                    <MenuIcon />
+                </IconButton>
+                <InputBase className={classes.input} placeholder="搜尋地址" inputProps={{"aria-label": "search google maps"}} />
+                <IconButton className={classes.iconButton} aria-label="search">
+                    <SearchIcon />
+                </IconButton>
+                <Divider className={classes.divider} orientation="vertical" />
+                <IconButton color="primary" className={classes.iconButton} aria-label="directions">
+                    <DirectionsIcon />
+                </IconButton>
+                <IconButton onClick={this.handleLocateMeClick} color="primary" className={classes.iconButton} aria-label="directions">
+                    <CenterFocusStrongIcon />
+                </IconButton>
+                <IconButton onClick={this.handleSideBySideModeClick} color="primary" className={classes.iconButton} aria-label="directions">
+                    <StreetviewIcon />
+                </IconButton>
             </Paper>
         );
     }
