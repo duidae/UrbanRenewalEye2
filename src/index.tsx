d182460015e6e2e49199e74d4b9620224ea70ed1
duidae <duidae@gmail.com>
2021-09-04 23:01:14 +0800

    Add fps meter

--- src/index.tsx
+++ src/index.tsx
@@ -15,3 +15,25 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// Add FPS meter for development
+if (process.env.NODE_ENV === "development") {
+    import("stats-js")
+        .then(({default: Stats}) => {
+            const stats = new Stats();
+            stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
+            document.body.appendChild(stats.dom);
+            function animate() {
+                stats.begin();
+                // monitored code goes here
+                stats.end();
+                requestAnimationFrame(animate);
+            }
+            requestAnimationFrame(animate);
+            stats.dom.style.right = "0";
+            stats.dom.style.left = "initial";
+        })
+        .catch(err => {
+            console.log(err);
+        });
+}
